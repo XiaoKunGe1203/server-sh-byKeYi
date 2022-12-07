b6665d3cfe75f8c8fb1cf1a4022d9303c5921841
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2022-12-07 09:40:23 +0800

    安装wget

---
+++ install.sh
@@ -0,0 +1,31 @@
+echo -------------------------------
+echo 欢迎使用一键开服脚本
+echo 
+echo 本脚本一键KeYi server(包括java17)
+echo 
+echo 请确保你已同意eula协议https://account.mojang.com/documents/minecraft_eula
+echo -----------------------------------
+sleep 30s
+echo 
+echo -------------------------------
+echo 请确保你已同意eula协议https://account.mojang.com/documents/minecraft_eula
+echo -----------------------------------
+echo 
+sleep 30s
+echo -------------------------------
+echo 请确保你已同意eula协议https://account.mojang.com/documents/minecraft_eula
+echo -----------------------------------
+echo 
+sleep 30s
+echo -------------------------------
+echo 请确保你已同意eula协议https://account.mojang.com/documents/minecraft_eula
+echo -----------------------------------
+
+cd ~
+mkdir KeYiServer
+cd KeYiServer
+echo --------------
+echo 安装wget
+echo ---------------
+
+yum install -y wget
