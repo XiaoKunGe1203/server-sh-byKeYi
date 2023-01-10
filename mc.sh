ccf451067b334e4d8e8e5f3c1237a6dfcbf8450c
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-01-10 10:24:59 +0800

    Create mc.sh

---
+++ mc.sh
@@ -0,0 +1,16 @@
+cd /opt/sh-server--KeYi-wj
+echo --------------
+echo 启动服务端中
+echo ---------------
+
+jdk-17.0.5/bin/java -jar keyi-main-1.19.2-18.jar
+
+echo ---------------------
+echo 服务端已关闭
+echo
+echo 再一次启动请输入  sh /mc.sh
+echo 
+echo 你的文件存放在/opt/sh-server--KeYi-wj/
+echo
+echo 更多帮助https://github.com/XiaoKunGe1203/server-sh-byKeYi/blob/main/help.md
+echo ------------------------
